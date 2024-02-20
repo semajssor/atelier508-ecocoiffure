e4d6dcdabddb4e8ef59efde63ddd50f67e788a84
semajssor <141041402+semajssor@users.noreply.github.com>
2024-02-20 23:34:25 +0300

    update

--- index.js
+++ index.js
@@ -34,20 +34,24 @@ function toggleInput() {
         }
 }
 
-function scrollToTop() {
-	document.body.scrollTop = 0; // For Safari
-	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
-      }
+var nav = document.querySelector('nav');
+window.onscroll = function() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        nav.classList.add('collapsed');
+        } else {
+        nav.classList.remove('collapsed');
+        }
+};
+
+function scrollFunction() {
+	const button = document.querySelector('.back-to-top-button'); // Updated selector
       
-      window.onscroll = function () {
-	scrollFunction();
-      };
-      
-      function scrollFunction() {
-	const button = document.querySelector('button');
 	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 	  button.style.display = 'block';
 	} else {
 	  button.style.display = 'none';
 	}
 }
+      
+
+
